da32cdd66fbf1633aa304873c7e3daa9bc08bf66
SAC-CS112-VasqueZ-JonathaN <jon088@hotmail.com>
2015-04-23 19:43:06 -0700

    sec

---
+++ src/diceMainClass.java
@@ -0,0 +1,16 @@
+
+
+public class diceMainClass {
+
+	public static void main(String[] args)
+	{
+		diceClass1 initialQuestion = new diceClass1();
+		initialQuestion.displayMessage();
+		
+		diceClass2 initialQuestion2 = new diceClass2();
+		initialQuestion2.displayMessage();
+		
+		
+		
+	}
+}
